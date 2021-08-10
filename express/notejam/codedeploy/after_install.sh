9f8015fbde1ff2b39eb0c2f7e8535253150ffd53
Laszlo Bodor <laci@lacisoft.ro>
2021-08-10 23:50:25 +0300

    fix after install issues

--- express/notejam/codedeploy/after_install.sh
+++ express/notejam/codedeploy/after_install.sh
@@ -1,4 +1,3 @@
-  
 #!/bin/bash
 
 # This script is executed after the source is copied to the instances
@@ -10,14 +9,11 @@ echo -n "DB_HOST=" > /usr/share/notejam/.env
 cat /home/ec2-user/endpoint.txt >> /usr/share/notejam/.env
 echo "" >> /usr/share/notejam/.env
 echo -n "DB_PASSWORD=" >> /usr/share/notejam/.env
-aws --region=$EC2_REGION ssm get-parameter --name "/Notejam/Database/DBPassword" --with-decryption --output text --query Parameter.Value >> /usr/share/notejam
-/.env
+aws --region=$EC2_REGION ssm get-parameter --name "/Notejam/Database/DBPassword" --with-decryption --output text --query Parameter.Value >> /usr/share/notejam/.env
 echo -n "DB_USERNAME=" >> /usr/share/notejam/.env
-aws --region=$EC2_REGION ssm get-parameter --name "/Notejam/Database/DBUser" --with-decryption --output text --query Parameter.Value >> /usr/share/notejam/.en
-v
+aws --region=$EC2_REGION ssm get-parameter --name "/Notejam/Database/DBUser" --with-decryption --output text --query Parameter.Value >> /usr/share/notejam/.env
 echo -n "DB_NAME=" >> /usr/share/notejam/.env
-aws --region=$EC2_REGION ssm get-parameter --name "/Notejam/Database/DBName" --with-decryption --output text --query Parameter.Value >> /usr/share/notejam/.en
-v
+aws --region=$EC2_REGION ssm get-parameter --name "/Notejam/Database/DBName" --with-decryption --output text --query Parameter.Value >> /usr/share/notejam/.env
 
 cp /tmp/ecosystem.config.js /usr/share/notejam/
 cd /usr/share/notejam && node db.js
